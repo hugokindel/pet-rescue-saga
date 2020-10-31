b30f1bb7dd03eabe6e626320d019521332c8211e
KINDEL Hugo <hugokindel.pro@protonmail.com>
2020-10-31 13:59:12 +0100

    Fix typo

--- src/com/g10/prs/Program.java
+++ src/com/g10/prs/Program.java
@@ -6,7 +6,7 @@ import com.g10.prs.command.PetRescueSaga;
 public class Program {
     /** Start of the program. */
     public static void main(String[] args) {
-        // Exit with the error code returned by the Visulog command.
+        // Exit with the error code returned by the Pet Rescue Saga command.
         System.exit(new PetRescueSaga().run(args));
     }
 }
