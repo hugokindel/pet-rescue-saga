0d5f06344f2bbfa498c3c2dc4e0515e970c0de1b
KINDEL Hugo <hugokindel.pro@protonmail.com>
2020-10-31 15:08:26 +0100

    Small fix

--- src/com/g10/prs/game/PetRescueSaga.java
+++ src/com/g10/prs/game/PetRescueSaga.java
@@ -14,14 +14,16 @@ public class PetRescueSaga extends Runnable {
     public int run(String[] args) {
         readArguments(args, PetRescueSaga.class);
 
-        System.out.println("   _____     _     _____                              _____                   ");
-        System.out.println("  |  __ \\   | |   |  __ \\                            / ____|                  ");
-        System.out.println("  | |__) |__| |_  | |__) |___  ___  ___ _   _  ___  | (___   __ _  __ _  __ _ ");
-        System.out.println("  |  ___/ _ \\ __| |  _  // _ \\/ __|/ __| | | |/ _ \\  \\___ \\ / _` |/ _` |/ _` |");
-        System.out.println("  | |  |  __/ |_  | | \\ \\  __/\\__ \\ (__| |_| |  __/  ____) | (_| | (_| | (_| |");
-        System.out.println("  |_|   \\___|\\__| |_|  \\_\\___||___/\\___|\\__,_|\\___| |_____/ \\__,_|\\__, |\\__,_|");
-        System.out.println("                                                                   __/ |      ");
-        System.out.println("                                                                  |___/       ");
+        if (!showHelp && !showVersion) {
+            System.out.println("   _____     _     _____                              _____                   ");
+            System.out.println("  |  __ \\   | |   |  __ \\                            / ____|                  ");
+            System.out.println("  | |__) |__| |_  | |__) |___  ___  ___ _   _  ___  | (___   __ _  __ _  __ _ ");
+            System.out.println("  |  ___/ _ \\ __| |  _  // _ \\/ __|/ __| | | |/ _ \\  \\___ \\ / _` |/ _` |/ _` |");
+            System.out.println("  | |  |  __/ |_  | | \\ \\  __/\\__ \\ (__| |_| |  __/  ____) | (_| | (_| | (_| |");
+            System.out.println("  |_|   \\___|\\__| |_|  \\_\\___||___/\\___|\\__,_|\\___| |_____/ \\__,_|\\__, |\\__,_|");
+            System.out.println("                                                                   __/ |      ");
+            System.out.println("                                                                  |___/       ");
+        }
 
         return 0;
     }
