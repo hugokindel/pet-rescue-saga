1c9678e97cf0d4bd4b9c819bc1078215a5d1b098
JAUROYON maxime <maxime.jauroyon@gmail.com>
2020-11-13 11:31:22 +0100

    tentative debut menu principal

--- src/com/g10/prs/PetRescueSaga.java
+++ src/com/g10/prs/PetRescueSaga.java
@@ -5,6 +5,9 @@ import com.g10.prs.core.cli.type.Runnable;
 import com.g10.prs.level.Level;
 import com.g10.prs.view.ViewType;
 import com.g10.prs.core.printer.Out;
+import com.g10.prs.player.Player;
+
+import java.util.Scanner;
 
 @Command(name = "prs", version = "1.0.0", description = "A game about rescuing animals.")
 public class PetRescueSaga extends Runnable {
@@ -25,6 +28,33 @@ public class PetRescueSaga extends Runnable {
             Out.println();
 
             // TODO: Menu principal
+
+
+            /**
+            Player player = new Player();
+            boolean verif = true;
+            Scanner sc = new Scanner(System.in);
+            while (verif) {
+                Out.println("Welcome " + player.getName() + " , what do you wnat to do ?");
+                Out.println("(p) Play ?  /   (q) Leave ?  /  (n) Choose a different name ?");
+                char result = player.choice(sc);
+                switch (result) {
+                    case 'p':
+                        verif = false;
+                        // Level with Player in param to change the player stat
+                        break;
+                    case 'q':
+                        sc.close();
+                        return 0;
+                    case 'n':
+                        player.newName(sc); //Redo the same Question
+                        break;
+                    default:
+                        Out.println("Wrong char");
+                        break;
+                }
+            } */
+
             try {
                 Level level = new Level("1.level");
             } catch (Exception e) {
---
+++ src/com/g10/prs/player/Player.java
@@ -0,0 +1,4 @@
+package com.g10.prs.player;
+
+public class Player {
+}
