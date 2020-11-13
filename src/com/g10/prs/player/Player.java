e17a0ba15c4b468753b01dcdf8ad78b2d9fdb9d2
JAUROYON maxime <maxime.jauroyon@gmail.com>
2020-11-13 11:32:18 +0100

    Player class wwith basic implementation

--- src/com/g10/prs/player/Player.java
+++ src/com/g10/prs/player/Player.java
@@ -1,4 +1,38 @@
 package com.g10.prs.player;
 
+import com.g10.prs.core.printer.Out;
+
+import java.util.Scanner;
+
 public class Player {
+    private String name;
+
+    public Player(){
+        name = "Player";
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void newName(Scanner sc){
+        Out.println("Write your new name");
+        if(sc.hasNext()) {
+            name = sc.next();
+            Out.println("Your new name is : " + name);
+        }
+    }
+
+    public char choice(Scanner sc){
+        if(sc.hasNext()) {
+            char result = sc.next().charAt(0);
+            return result;
+        }
+        return 'e';
+    }
+
+
+
+
+
 }
