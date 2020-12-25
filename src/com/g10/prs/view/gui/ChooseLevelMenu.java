386bf637e3d90a9e028185db009bb00d2b62ced9
KINDEL Hugo <hugokindel.pro@protonmail.com>
2020-12-25 12:55:35 +0100

    Update GUI

--- src/com/g10/prs/view/gui/ChooseLevelMenu.java
+++ src/com/g10/prs/view/gui/ChooseLevelMenu.java
@@ -1,14 +1,50 @@
 package com.g10.prs.view.gui;
 
+import com.g10.prs.PetRescueSaga;
+import com.g10.prs.common.Pair;
+import com.g10.prs.common.Resources;
+import com.g10.prs.level.Level;
+
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChooseLevelMenu extends GuiMenu {
+    List<String> levels;
+
     public ChooseLevelMenu() {
         super("Choisir un niveau");
     }
 
     @Override
     protected void drawContent() {
-        
+        JPanel contentPanel = new JPanel();
+        levels = new ArrayList<>();
+        levels.addAll(Resources.getCampaignLevelsList());
+
+        String[] levelNames = new String[levels.size()];
+
+        for (int i = 0; i < levelNames.length; i++) {
+            try {
+                levelNames[i] = Level.load(Resources.getLevelsDirectory() + "/" + levels.get(i)).getName();
+                JButton quitButton = new JButton(levelNames[i]);
+                int finalI = i;
+                quitButton.addActionListener(ev -> {
+                    try {
+                        PetRescueSaga.level = Level.load(Resources.getLevelsDirectory() + "/" + levels.get(finalI));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    PetRescueSaga.view.changeMenu(new PlayLevelMenu());
+                });
+                contentPanel.add(quitButton);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        panel.add(contentPanel);
     }
 }
---
+++ src/com/g10/prs/view/gui/PlayLevelMenu.java
@@ -0,0 +1,14 @@
+package com.g10.prs.view.gui;
+
+import com.g10.prs.PetRescueSaga;
+
+public class PlayLevelMenu extends GuiMenu {
+    public PlayLevelMenu() {
+        super(PetRescueSaga.level.getName());
+    }
+
+    @Override
+    protected void drawContent() {
+
+    }
+}
