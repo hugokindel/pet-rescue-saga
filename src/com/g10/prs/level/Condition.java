abfb08bb7ad888853d7fc6a0c6e7480117798b8c
KINDEL Hugo <hugokindel.pro@protonmail.com>
2020-12-26 18:52:35 +0100

    Update documentation

--- src/com/g10/prs/level/Condition.java
+++ src/com/g10/prs/level/Condition.java
@@ -5,22 +5,37 @@ import com.g10.prs.njson.NJsonSerializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A Condition is a specific type which defines a condition to verify.
+ * It is used in the Group system, to permit refill with the use of Conditions.
+ */
 @NJsonSerializable
 public class Condition {
+    /** The name of the condition. */
     @NJsonSerializable
     private String name;
 
+    /** The values to verify with. */
     @NJsonSerializable
     private List<String> values;
 
+    /** @return the name of the condition. */
     public String getName() {
         return name;
     }
 
+    /** @return the list of values to verify with. */
     public List<String> getValues() {
         return values;
     }
 
+    /**
+     * Checks if the condition is true or not.
+     *
+     * @param nameToCompare Name to verify.
+     * @param valueToCompare Value to verify.
+     * @return if it is true.
+     */
     public boolean isTrue(String nameToCompare, String valueToCompare) {
         if (!name.equals(nameToCompare)) {
             return false;
@@ -35,7 +50,12 @@ public class Condition {
         return false;
     }
 
-    public Condition copy(){
+    /**
+     * Creates a deep copy the Condition.
+     *
+     * @return the copy.
+     */
+    public Condition copy() {
         Condition res = new Condition();
         res.name = name;
         res.values = new ArrayList<>(values);
--- src/com/g10/prs/level/Group.java
+++ src/com/g10/prs/level/Group.java
@@ -5,36 +5,53 @@ import com.g10.prs.njson.NJsonSerializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A group is a list of block IDs which can be used within a level implementation
+ * to permit to use random choice of blocks when generating, or the refill mechanism.
+ */
 @NJsonSerializable
 public class Group {
+    /** The ID of the group. */
     @NJsonSerializable
     private int id;
 
+    /** The list of block IDs. */
     @NJsonSerializable
     private List<Integer> blocks;
 
+    /** If it supports refill (new blocks falling from the top of the level when possible). */
     @NJsonSerializable(necessary = false)
     private boolean canRefill;
 
+    /** Condition to verify when using refill, to check if we can refill at a specific position. */
     @NJsonSerializable(necessary = false)
     Condition refillCondition;
 
+    /** @return the ID of the group. */
     public int getId() {
         return id;
     }
 
+    /** @return the list of block IDs. */
     public List<Integer> getBlocks() {
         return blocks;
     }
 
+    /** @return if it can refill. */
     public boolean canRefill() {
         return canRefill;
     }
 
+    /** @return the refill condition. */
     public Condition getRefillCondition() {
         return refillCondition;
     }
 
+    /**
+     * Creates a deep copy the Group.
+     *
+     * @return the copy.
+     */
     public Group copy(){
         Group res = new Group();
         res.id = id;
