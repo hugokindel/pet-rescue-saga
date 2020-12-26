a98579ce212be344222e48a9a21e2a8af6dad959
JAUROYON maxime <maxime.jauroyon@gmail.com>
2020-12-26 15:07:09 +0100

    Add comment

--- src/com/g10/prs/power/Power.java
+++ src/com/g10/prs/power/Power.java
@@ -1,6 +1,6 @@
 package com.g10.prs.power;
 
-/** class to create a power */
+/** base class of every power */
 public abstract class Power {
     /** action of the power */
     public abstract void use();
--- src/com/g10/prs/view/View.java
+++ src/com/g10/prs/view/View.java
@@ -2,27 +2,37 @@ package com.g10.prs.view;
 
 import java.util.Stack;
 
+/** base class of every view */
 public class View {
+    /** current menu */
     protected Menu currentMenu;
+
+    /** Stack of all the previous menu */
     protected Stack<Menu> menuBacklog;
+
+    /** to quit the view */
     protected boolean quit;
 
+    /** class constructor */
     public View(Menu defaultMenu) {
         currentMenu = defaultMenu;
         menuBacklog = new Stack<>();
         quit = false;
     }
 
+    /** to quit the view */
     public void quit() {
         quit = true;
     }
 
+    /** to go to the previous menu */
     public void goBack() {
         if (!menuBacklog.empty()) {
             currentMenu = menuBacklog.pop();
         }
     }
 
+    /** to run the game */
     public void run() {
         while (!quit) {
             currentMenu.draw();
@@ -38,6 +48,11 @@ public class View {
         }
     }
 
+    /**
+     *  change the current menu
+     *
+     * @param addCurrentToBacklog decide if we can go back to the previous menu 
+     */
     public void changeMenu(Menu menu, boolean addCurrentToBacklog) {
         if (addCurrentToBacklog) {
             menuBacklog.push(currentMenu);
@@ -46,6 +61,7 @@ public class View {
         currentMenu = menu;
     }
 
+    /** change the current menu with the possibility to go back */
     public void changeMenu(Menu menu) {
         changeMenu(menu, true);
     }
